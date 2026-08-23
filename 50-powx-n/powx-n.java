59a56a249e2ac6cded84cda5717e18c359f15eb7
Sanapala Saimani <saimani.23bce9345@vitapstudent.ac.in>
2026-08-23 23:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (47.53%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(n<1){x=1/x; n=-n;}
+        double pow=1;
+        while(n!=0){
+            if((n&1)!=0){
+                pow*=x;
+                n=n-1;
+            }else{
+                x*=x;
+                n>>>=1;
+            }
+        }
+        return pow;
     }
 }
