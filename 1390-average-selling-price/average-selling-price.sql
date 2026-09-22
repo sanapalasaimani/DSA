1793364544fdad5402b480d1b5626875f755003d
Sanapala Saimani <saimani.23bce9345@vitapstudent.ac.in>
2026-09-22 16:24:48 +0530

    Time: 876 ms (64.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,11 @@
+SELECT 
+    p.product_id,
+    ROUND(
+        COALESCE(SUM(s.units * p.price) / SUM(s.units), 0),
+        2
+    ) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold s
+    ON p.product_id = s.product_id
+    AND s.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
